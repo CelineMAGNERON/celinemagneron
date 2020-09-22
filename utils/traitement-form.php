fb2cb054e80c480edaca95ec11f8a585a7d5e084
Cel Magneron <magneron.cel@gmail.com>
2020-09-22 16:31:19 +0200

    :loud_sound:search env var

--- utils/traitement-form.php
+++ utils/traitement-form.php
@@ -45,14 +45,14 @@ try {
     $mail->isSMTP(); // Send using SMTP
     $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = $_ENV['MAILADRESS']; // SMTP username
-    $mail->Password = $_ENV['PASSWORD']; // SMTP password
+    $mail->Username = getenv('MAILADRESS'); // SMTP username
+    $mail->Password = getenv('PASSWORD'); // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
     //Recipients
     $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress($_ENV['MAILADRESS']); // Name is optional
+    $mail->addAddress(getenv('MAILADRESS')); // Name is optional
     
     // Content
     $mail->isHTML(true); // Set email format to HTML
