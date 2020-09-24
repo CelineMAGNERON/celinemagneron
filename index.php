38935b81e9552fc9e200f3e66f66bf25673a6f71
Cel Magneron <magneron.cel@gmail.com>
2020-09-24 22:01:15 +0200

    :lipstick:Anim'maincard

--- index.php
+++ index.php
@@ -32,14 +32,16 @@
 
     <!-- Section Qui suis-je -->
     <section id="who">
-        <h3>Qui suis-je ?</h3>
-        <p>Enfant des années fin 90, depuis la campagne où j'ai grandi, j'étais insatiablement attirée par cette toile naissante qu'est le World Wide Web. Pourquoi s'occuper d'un Tamagochi alors qu'on peut s'investir à convaincre ses parents à équiper la maison d'un ordinateur (avec une connexion évidemment, merci AOL !) ? 
-        <p>Plus tard, après 8 ans à évoluer dans le secteur professionnel du social, il est temps de revenir à ce centre d'intérêt de cette drôlesse (pour parler poitevin) 'millenniale'.</p>
-        <p>Mon projet professionnel est de devenir développeuse full-stack JavaScript, tout en sensibilisant à l’accessibilité du web pour tout internaute, et me spécialiser dans l’éco-conception des projets afin de minimiser l’impact du web sur l’environnement.</p>
-        <p>J'ai débuté par une formation intensive à l'ENI Ecole Informatique de niveau 5. Cependant afin d’optimiser la qualité de mon parcours auprès de professionnels, j’ai intègré le Bachelor Développeuse web en alternance à l’ICSSA de Niort et en partenariat avec 3iL Ingénieurs de Limoges, pour une durée d’un an, en septembre 2020 : 
-        <a class="button" href="https://www.icssa-niort.com/organisation-pedagogique-formations-icssa/centre-de-formation/le-bachelor-web-developpeur/" title="ICSSA" target="_blank">Voir le descriptif de la formation</a>.</p>
-        <p>Je suis à la recherche d'une structure dynamique et tournée vers des technologies toujours actualisées dans laquelle investir mes compétences et ma créativité sur un poste de développeuse JavaScript, et restant ouverte aux postes J2EE  :)</p>
-        <p>Durant ces huit dernières années à évoluer dans le secteur du social, j’ai développé de solides capacités pour travailler en équipe, développer mon sens de l’adaptation et l’autodidaxie. Je suis sensible aux valeurs Humaines, du sens poétique au sens éthique, et par conséquent à l’ergonomie et à l’accessibilité du web.</p>
+        <div>
+            <h3>Qui suis-je ?</h3>
+            <p>Enfant des années fin 90, depuis la campagne où j'ai grandi, j'étais insatiablement attirée par cette toile naissante qu'est le World Wide Web. Pourquoi s'occuper d'un Tamagochi alors qu'on peut s'investir à convaincre ses parents à équiper la maison d'un ordinateur (avec une connexion évidemment, merci AOL !) ? 
+            <p>Plus tard, après 8 ans à évoluer dans le secteur professionnel du social, il est temps de revenir à ce centre d'intérêt de cette drôlesse (pour parler poitevin) 'millenniale'.</p>
+            <p>Mon projet professionnel est de devenir développeuse full-stack JavaScript, tout en sensibilisant à l’accessibilité du web pour tout internaute, et me spécialiser dans l’éco-conception des projets afin de minimiser l’impact du web sur l’environnement.</p>
+            <p>J'ai débuté par une formation intensive à l'ENI Ecole Informatique de niveau 5. Cependant afin d’optimiser la qualité de mon parcours auprès de professionnels, j’ai intègré le Bachelor Développeuse web en alternance à l’ICSSA de Niort et en partenariat avec 3iL Ingénieurs de Limoges, pour une durée d’un an, en septembre 2020 : 
+            <a class="button" href="https://www.icssa-niort.com/organisation-pedagogique-formations-icssa/centre-de-formation/le-bachelor-web-developpeur/" title="ICSSA" target="_blank">Voir le descriptif de la formation</a>.</p>
+            <p>Je suis à la recherche d'une structure dynamique et tournée vers des technologies toujours actualisées dans laquelle investir mes compétences et ma créativité sur un poste de développeuse JavaScript, et restant ouverte aux postes J2EE  :)</p>
+            <p>Durant ces huit dernières années à évoluer dans le secteur du social, j’ai développé de solides capacités pour travailler en équipe, développer mon sens de l’adaptation et l’autodidaxie. Je suis sensible aux valeurs Humaines, du sens poétique au sens éthique, et par conséquent à l’ergonomie et à l’accessibilité du web.</p>
+        </div>
         <div class="logo">
             <a href="https://www.linkedin.com/in/c%C3%A9line-magneron-861253138/" rel="" target="_blank">
                 <img class="logo" src="./assets/images/linkedin.png" alt="Jetez un oeil à mon profi linkedIn" title="Jetez un oeil à mon profi linkedIn">
